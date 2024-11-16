f01ec26ad187fb90b9e3070a3b3ed832f78ba2c0
Charlie Ludowici <charlie.ludowici@gmail.com>
2024-11-17 10:58:35 +1100

    move base path

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Hamiltonian Monte Carlo",
+        "excerpt":"$\\renewcommand{\\hat}[1]{\\widehat{#1}}$ # %conda install tqdm==4.66.5 scipy==1.13.1 plotly==5.24.1 numpy==1.26.4 pandas==2.2.2 openblas==0.3.21 matplotlib import numpy as np # import plotly.graph_objects as go # from plotly.subplots import make_subplots import matplotlib.pyplot as plt from tqdm import tqdm from scipy.stats import norm, multivariate_normal, uniform, beta from samplers import hmc, metro_hastings, R_hat, plot_chains, param_scatter n_iters =...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/Linear-Regression/",
+        "teaser": null
+      }]
